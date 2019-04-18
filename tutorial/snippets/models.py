ef48f32317a5d7997e1402a3b07032f11601734b
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2019-04-18 23:22:08 +0900

    Setup snippets model and serializer
    - Add snippets migration
    - Add snippets serializer

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -17,13 +17,9 @@ class Snippet(models.Model):
     code = models.TextField()
     line_numbers = models.BooleanField(default=False)
     language = models.CharField(
-        choices=LANGUAGE_CHOICES,
-        default="python",
-        max_length=100)
-    styles = models.CharField(
-        choices=STYLE_CHOICES,
-        default='friendly',
-        max_length=100)
+        choices=LANGUAGE_CHOICES, default="python", max_length=100
+    )
+    style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
 
     class Meta:
-        ordering = ('created',)
+        ordering = ("created",)
